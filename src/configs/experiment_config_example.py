29d111b9e1658a83dd2c83591cd59b71610193cb
erobitschek <erobitschek@gmail.com>
2023-10-07 20:44:13 -0600

    add class names to example config

--- src/configs/experiment_config_example.py
+++ src/configs/experiment_config_example.py
@@ -17,6 +17,7 @@ config = RunConfig(
         path="../data/synth_med_data/raw/synth_med_data_400patients_05women_01target_015femalecodes_015malecodes.csv",
         split_ratios=SplitRatios(train=0.8, val=0.1, test=0.1),
         target="is_Female",
+        class_names = ["Male", "Female"], 
         feature_threshold=5,
     ),
 )
